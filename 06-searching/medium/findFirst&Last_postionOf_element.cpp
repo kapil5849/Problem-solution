a5a07240ec2c57a400ace98ff7fb1430577b2afa
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-11-02 14:56:56 +0530

    Update findFirst&Last_postionOf_element.cpp

--- 06-searching/medium/findFirst&Last_postionOf_element.cpp
+++ 06-searching/medium/findFirst&Last_postionOf_element.cpp
@@ -1,8 +1,6 @@
 
 // 34. Find First and Last Position of Element in Sorted Array
 
-# [34. Find First and Last Position of Element in Sorted Array](https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/)
-
 
 class Solution {
  public:
