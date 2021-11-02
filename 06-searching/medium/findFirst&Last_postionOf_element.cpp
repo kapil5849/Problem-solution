658562a8ab023835acb1e5339a29efc28cbfb9f1
kapil vaishnav <77320872+kapil5849@users.noreply.github.com>
2021-11-02 15:03:00 +0530

    Update findFirst&Last_postionOf_element.cpp

--- 06-searching/medium/findFirst&Last_postionOf_element.cpp
+++ 06-searching/medium/findFirst&Last_postionOf_element.cpp
@@ -5,15 +5,16 @@
 class Solution {
  public:
   vector<int> searchRange(vector<int>& nums, int target) {
-    const int start = firstGreaterEqual(nums, target);
-    if (start == nums.size() || nums[start] != target)
+    int n=nums.size();
+    const int st = firstIndex(nums, target);
+    if (st == n || nums[st] != target)
       return {-1, -1};
-    const int end = firstGreaterEqual(nums, target + 1) - 1;
-    return {start, end};
+    const int end = firstIndex(nums, target + 1) - 1;
+    return {st, end};
   }
 
  private:
-    int firstGreaterEqual(const vector<int>& A, int target) {
+    int firstIndex(const vector<int>& A, int target) {
     return lower_bound(begin(A), end(A), target) - begin(A);
   }
 };
